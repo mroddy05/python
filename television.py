98099a7a881e1366b5836ea4f7a561e7c7971c32
mroddy05 <mroddy@unomaha.edu>
2024-04-19 13:44:25 -0500

    Finished television.py and test_television.py

--- television.py
+++ television.py
@@ -1,42 +1,61 @@
 class Television:
-    min_volume = 0
-    max_volume = 2
-    min_channel = 0
-    max_channel = 3
+    min_volume: int = 0
+    max_volume: int = 2
+    min_channel: int = 0
+    max_channel: int = 3
 
-    def __init__(self):
-        self.__status = False
-        self.__muted = False
-        self.__volume = self.min_volume
-        self.__channel = self.min_channel
+    def __init__(self) -> None:
+        '''
+        Start the initial variables for the Television object
+        '''
+        self.__status: bool = False
+        self.__muted: bool = False
+        self.__volume: int = self.min_volume
+        self.__channel: int = self.min_channel
 
-    def power(self):
+    def power(self) -> None:
+        '''
+        Toggle the power status
+        '''
         if self.__status == False:
             self.__status = True
         else:
             self.__status = False
 
-    def mute(self):
+    def mute(self) -> None:
+        '''
+        Toggle the muted status
+        '''
         if self.__muted == True:
             self.__muted = False
         else:
             self.__muted = True
 
-    def channel_up(self):
+    def channel_up(self) -> None:
+        '''
+        Iterates the channel up by one, wrapping to the beginning when reaching the end
+        '''
         if self.__status == True:
             if self.__channel == self.max_channel:
                 self.__channel = self.min_channel
             else:
                 self.__channel += 1
 
-    def channel_down(self):
+    def channel_down(self) -> None:
+        '''
+        Iterates the channel down by one, wrapping to the end when reaching the beginning
+        '''
         if self.__status == True:
             if self.__channel == self.min_channel:
                 self.__channel = self.max_channel
             else:
                 self.__channel -= 1
 
-    def volume_up(self):
+    def volume_up(self) -> None:
+        '''
+        Iterates the volume up by one, wrapping to the beginning when reaching the end. Does not change when the
+        television status is False or off and unmutes if already muted.
+        '''
         if self.__status == True:
             if self.__muted == True:
                 self.mute()
@@ -45,7 +64,11 @@ class Television:
             else:
                 self.__volume += 1
 
-    def volume_down(self):
+    def volume_down(self) -> None:
+        '''
+        Iterates the volume down by one, wrapping to the end when reaching the beginning. Does not change when the
+        television status is False or off and unmutes if already muted.
+        '''
         if self.__muted == True:
             self.mute()
         if self.__status == True:
@@ -54,5 +77,12 @@ class Television:
             else:
                 self.__volume -= 1
 
-    def __str__(self):
-        return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
+    def __str__(self) -> str:
+        '''
+        Retrieve the status, channel, and volume variables of the television object
+        :return: a string representing the certain television object
+        '''
+        if self.__muted == False:
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
+        else:
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.min_volume}'
